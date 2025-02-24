5429eb648f3e7b30b31b7ecc0dc87832c651d33a
Daniel <danielelacer08@gmail.com>
2025-02-24 13:30:56 +0000

    295

---
+++ 150/0295-find-mean-from-data-stream.py
@@ -0,0 +1,21 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.left = []
+        self.right = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.left, -num)
+        
+        if self.left and self.right and (-self.left[0] > self.right[0]):
+            heapq.heappush(self.right, -heapq.heappop(self.left))
+        
+        if len(self.left) > len(self.right) + 1:
+            heapq.heappush(self.right, -heapq.heappop(self.left))
+        if len(self.right) > len(self.left):
+            heapq.heappush(self.left, -heapq.heappop(self.right))
+
+    def findMedian(self) -> float:
+        if len(self.left) > len(self.right):
+            return -self.left[0]
+        return (-self.left[0] + self.right[0]) / 2.0
