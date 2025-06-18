8b8d99115b16f8db20bc9f6eeec51cc361ebb646
marcbeep <77645923+marcbeep@users.noreply.github.com>
2025-06-18 16:47:41 +0100

    best time, valid palindrome, invert b tree, valid anagram, binary search:

--- src/lib/generated-problem-imports.ts
+++ src/lib/generated-problem-imports.ts
@@ -4,9 +4,14 @@
 import type { LeetCodeProblem } from "@/lib/types";
 
 import problem1 from "@/problems/json/001-two-sum.json";
-import problem2 from "@/problems/json/003-merge-two-sorted-lists.json";
-import problem3 from "@/problems/json/020-valid-parentheses.json";
+import problem2 from "@/problems/json/020-valid-parentheses.json";
+import problem3 from "@/problems/json/021-merge-two-sorted-lists.json";
+import problem4 from "@/problems/json/121-best-time-to-buy-and-sell-stock.json";
+import problem5 from "@/problems/json/125-valid-palindrome.json";
+import problem6 from "@/problems/json/226-invert-binary-tree.json";
+import problem7 from "@/problems/json/242-valid-anagram.json";
+import problem8 from "@/problems/json/704-binary-search.json";
 
-const allProblems = [problem1, problem2, problem3] as LeetCodeProblem[];
+const allProblems = [problem1, problem2, problem3, problem4, problem5, problem6, problem7, problem8] as LeetCodeProblem[];
 
 export { allProblems };
